da1b3fd6d8c82780c1c0a948a70a481b9b75b47f
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-08-20 23:09:33 -0300

    corrigindo gcloud script

--- GKE/Terraform/gcloud-auth.sh
+++ GKE/Terraform/gcloud-auth.sh
@@ -4,4 +4,4 @@ GCLOUD_PROJECT=mlopscase
 gcloud auth activate-service-account $GCLOUD_LOGIN_INFO \
  --key-file=key.json --project=$GCLOUD_PROJECT
 
- export GOOGLE_APPLICATION_CREDENTIALS=key.json
+export GOOGLE_APPLICATION_CREDENTIALS=key.json
