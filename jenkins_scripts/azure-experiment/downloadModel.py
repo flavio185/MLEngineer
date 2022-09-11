65a4e1323901cc94ded1f4279b3cd97db2e1ca7b
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-09-10 21:06:58 -0300

    adicionando download de modelo.

--- jenkins_scripts/azure-experiment/downloadModel.py
+++ jenkins_scripts/azure-experiment/downloadModel.py
@@ -3,14 +3,12 @@ from azure_experiment import AzureModel
 import sys
 
 
-
-from azure_experiment import AzureModel 
-
 #Main
 def downloadModel(model_name):
     model_name = model_name[-1]
     m1 = AzureModel()
-    m1.downloadModelArtifacts(model_name, target_dir="app", version=3)
+    #m1.downloadModelArtifacts(model_name, target_dir="app", version=3)
+    m1.downloadModelArtifacts(model_name, target_dir="app")
 
 if __name__ == "__main__":
     downloadModel(sys.argv)
---
+++ jenkins_scripts/conteinerize-ml-model.sh
@@ -0,0 +1,23 @@
+
+#Clone project
+git clone $GIT_PROJECT
+export PROJECT_DIR=$(echo $GIT_PROJECT | awk -F/ '{print $NF}'| sed 's/.git//g')
+#Download model artifacts.
+git clone https://github.com/flavio185/MLEngineer.git
+sudo docker run \
+    -e AZURE_CLIENT_SECRET=$AZURE_CLIENT_SECRET \
+    -e AZURE_CLIENT_ID=$AZURE_CLIENT_ID \
+    -e RESOURCE_GROUP=$RESOURCE_GROUP \
+    -e WORKSPACE_NAME=$WORKSPACE_NAME \
+    -e AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
+    -e AZURE_TENANT_ID=$AZURE_TENANT_ID \
+    -e AZURE_STORAGE_ACCOUNT=$AZURE_STORAGE_ACCOUNT \
+    -v $PWD/MLEngineer/jenkins_scripts/azure-experiment:/azure-experiment \
+    python-az \
+    /bin/bash -c "cd /azure-experiment && \
+    python downloadModel.py $PROJECT_DIR
+    "
+
+mv output $PROJECT_DIR/API/app 
+
+sudo rm -rf $PROJECT_DIR MLEngineer
