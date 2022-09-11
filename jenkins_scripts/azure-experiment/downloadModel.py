0c09ad950966e02c232bb99b53c7c22a44554029
Flavio Venturini <flavio.venturini@f1rst.com.br>
2022-09-10 21:29:39 -0300

    adicionando download de modelo.

--- jenkins_scripts/azure-experiment/downloadModel.py
+++ jenkins_scripts/azure-experiment/downloadModel.py
@@ -4,11 +4,14 @@ import sys
 
 
 #Main
-def downloadModel(model_name):
-    model_name = model_name[-1]
+def downloadModel(args):
+    m_name = [ i.split('=')[1] for i in args if 'model_name' in i]
+    #t_dir = [ i.split('=')[1] for i in args if 'target_dir' in i]
+    ver = [ i.split('=')[1] for i in args if 'version' in i]
+
     m1 = AzureModel()
-    #m1.downloadModelArtifacts(model_name, target_dir="app", version=3)
-    m1.downloadModelArtifacts(model_name, target_dir="app")
+    #m1.downloadModelArtifacts(m_name, target_dir=t_dir, version=ver)
+    m1.downloadModelArtifacts(m_name)
 
 if __name__ == "__main__":
     downloadModel(sys.argv)
