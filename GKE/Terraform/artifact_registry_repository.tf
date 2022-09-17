35b7e6b25e88133717e53cc7d872eab699290644
flavio185 <flavio185@gmail.com>
2022-09-17 09:20:33 -0300

    Update artifact_registry_repository.tf

--- GKE/Terraform/artifact_registry_repository.tf
+++ GKE/Terraform/artifact_registry_repository.tf
@@ -1,6 +1,11 @@
-#GCP Artifactory Repository Registry
+provider "google" {
+    project = "PROJECT-ID"
+}
+
 resource "google_artifact_registry_repository" "my-repo" {
-  location      = var.region
+  provider = google-beta
+
+  location = var.region
   repository_id = "${var.project_id}-gke"
   description   = "Docker Registry for GKE"
   format        = "DOCKER"
