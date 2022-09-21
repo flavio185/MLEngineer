b802a2b1ac6516ab7a626fbed644abb400f1c72f
flavio185 <flavio185@gmail.com>
2022-09-20 22:43:29 -0300

    Update artifact_registry_repository.tf

--- GKE/Terraform/artifact_registry_repository.tf
+++ GKE/Terraform/artifact_registry_repository.tf
@@ -7,4 +7,9 @@
 #  format        = "DOCKER"
 #}
 
-
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "my-repository"
+  description   = "example docker repository"
+  format        = "DOCKER"
+}
