baa51401183df64836af307ffa22747797533af3
flavio185 <flavio185@gmail.com>
2022-09-21 07:32:47 -0300

    Update gke.tf

--- GKE/Terraform/gke.tf
+++ GKE/Terraform/gke.tf
@@ -3,9 +3,6 @@ resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
   
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
   remove_default_node_pool = false
   initial_node_count       = 1
 
