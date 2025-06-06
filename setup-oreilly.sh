c1e5361bab8b5a9efdce65b20d7a2cdf7b4b35ea
flavio185 <flavio185@gmail.com>
2025-06-06 00:04:23 -0300

    script

---
+++ setup-oreilly.sh
@@ -0,0 +1,19 @@
+#1 - Spin up a aks cluster
+az aks create \
+--resource-group $resource \
+--name MLWork \
+--node-count 3 \
+--generate-ssh-keys
+
+az aks get-credentials --resource-group $resource --name MLWork
+
+#2 - Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#3 - Clone git
+	
+#4 - Apply helm template
+cd helm-chart/mlflow-chart
+helm install mlflow . --namespace mlflow-ns --create-namespace 
